01b4ba21295485462108366eaa1775a95f82e49b
domrev_igor <igor@innovid.com>
2016-03-12 20:46:54 +0200

    typo in hook cask[4]

--- hooks.rb
+++ hooks.rb
@@ -11,6 +11,7 @@ class Hooks < Formula
     puts hooks
   unless File.exist?(hooks)
     puts Dir.entries('.')
+    puts Dir.pwd
     FileUtils.mv('./git-hooks', hooks)
     Dir.entries(hooks).reject { |file| %w(. ..).include?(file) or file[0..-1].include?('.') }.each { |hook|
       FileUtils.chmod('+x', hook) }
