816d134d68e6723dfab2e7860678de9b6a232526
domrev_igor <igor@innovid.com>
2016-03-12 20:41:01 +0200

    adding hooks cask

--- hooks.rb
+++ hooks.rb
@@ -1,7 +1,7 @@
 class Hooks < Formula
   desc 'usefull stuff and goodies'
   homepage 'https://github.com/viggin543/homebrew-innovidcellar'
-  url 'https://github.com/viggin543/homebrew-innovidcellar/archive/1.2.1.tar.gz'
+  url 'https://github.com/viggin543/homebrew-innovidcellar/archive/1.2.2.tar.gz'
   version '1.2.1'
   sha256 ''
 
@@ -10,7 +10,8 @@ class Hooks < Formula
     hooks = Dir.home+"/bin/git-hooks"
   unless File.exist?(hooks)
     FileUtils.mv('./git-hooks', hooks)
-    Dir.entries(hooks) { |hook| FileUtils.chmod('+x', hook) }
+    Dir.entries(hooks).reject { |file| %w(. ..).include?(file) or file[0..-1].include?('.') }.each
+    { |hook| FileUtils.chmod('+x', hook) }
   end
 
   end
